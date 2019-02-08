b95d38f15288c877646afb9781ded2666ac56e9e
tuteo97nd <tuteo.nd@gmail.com>
2019-02-08 16:42:34 +0700

    tu

---
+++ src/main/java/com/dts/cinema_demo/CinemaDemoApplication.java
@@ -0,0 +1,14 @@
+package com.dts.cinema_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaDemoApplication.class, args);
+    }
+
+}
+
